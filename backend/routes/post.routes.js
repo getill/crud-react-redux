401745ec14d5bc283c0c904b4bf5ad5a802f0693
getill <theo.lemray@gmail.com>
2023-08-18 18:29:23 +0200

    Server test with postman 🗃

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const port = 5000;
+
+// Logique d'un CRUD
+router.get("/", (req, res) => {
+  res.json({ message: "P'tit message" });
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: req.body.message });
+});
+
+router.put("/:id", (req, res) => {
+  res.json({ messageId: req.params.id });
+});
+
+router.delete("/:id", (req, res) => {
+  res.json({ message: "Post supprimé id : " + req.params.id });
+});
+
+router.patch("/like-post/:id", (req, res) => {
+  res.json({ message: "Post liké id : " + req.params.id });
+});
+
+router.patch("/dislike-post/:id", (req, res) => {
+  res.json({ message: "Post disliké id : " + req.params.id });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,11 @@ const port = 5000;
 
 const app = express();
 
+// Middleware (permet de traiter les données de la request "req")
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/post", require("./routes/post.routes"));
+
+// Run the server
 app.listen(port, () => console.log("Server run at" + port));

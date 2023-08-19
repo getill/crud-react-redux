7747c3f6d110d4e00e409b814d1adacc8e46971c
getill <theo.lemray@gmail.com>
2023-08-19 15:54:23 +0200

    CSS imported from an exemple 🎨

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [userId, setUserId] = useState("");
+
   return (
-    <div>
-      <h1>Hello react</h1>
+    <div className="app-container">
+      <div className="login">
+        <h3>Bonjour</h3>
+        <input
+          type="text"
+          placeholder="Pseudo"
+          onChange={(e) => setUserId(e.target.value)}
+        />
+      </div>
+      {userId}
     </div>
   );
 };
